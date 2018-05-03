9358f04dfe702c44afff9b6f21f1c597505dadb2
linzichao <joy10393@gmail.com>
2018-05-03 09:36:33 +0800

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_authentication_token
+
   def generate_authentication_token
     self.authentication_token = Devise.friendly_token
   end
