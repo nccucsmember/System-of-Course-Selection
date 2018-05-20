869d67feae514e718d93965cbf08d1378e8664a2
Jeff <jeffrey110616@gmail.com>
2018-05-20 23:59:24 +0800

    Return full tuple by string matching

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -31,14 +31,87 @@ class CourseController < ApplicationController
   	return Vector.elements(tmp_vec_lst)
   end
 
+  def course_name_fuzzysearch(query)
+    name_lst = []
+    db_courses = Course.all
+    courses = []
+    db_courses.each do |tuple|
+      courses << tuple
+    end
+    @name_hash = Course.select("course_name_ch").distinct
+    @name_hash.each do |n|
+      name_lst.push(n.course_name_ch)
+    end
+
+    # Build unique character lst
+    character_lst = []
+    name_lst.each { |word| word.split("").each { |c| character_lst << c  } }
+    character_lst = character_lst.uniq
+    counter = 0
+
+    # Build index mapping for vsm
+    index_mapping = {}
+    character_lst.each do |c|
+      index_mapping[c] = counter
+      counter += 1
+    end
+
+    #query clean
+    q = query
+    printf "Original Query: %s\n", q
+    penal = corpus_penalty(q, character_lst)
+    tmp_q = ""
+    q.split("").each {|c| if character_lst.include? c; tmp_q << c; end}
+    q = tmp_q
+    if tmp_q == ""
+      return courses
+    end
+
+    v2 = vectorize_tf(q, character_lst, index_mapping)
+    dt_score = {}
+    name_lst.each do |word|
+      dt_score[word] = sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2) * penal
+      #printf "%s: %.6f\n", word, sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2)
+    end
+
+    printf "Cleaned Query: %s\npenalty: %.5f\n\n", q, penal
+    lst_name_score = []
+    dt_score.each do |k, val|
+      tmp = [k, val]
+      lst_name_score.push( tmp )
+    end
+    re_lst = []
+    sorted_lst_name_score = lst_name_score.sort_by { |tuple| tuple[1] }.reverse
+    sorted_lst_name_score.each do |t|
+      if t[1] > 0.45
+        re_lst.push( t[0] )
+        printf "%s:   %.4f\n", t[0], t[1]
+      else
+        break
+      end
+    end
+
+    return_lst = []
+    courses.each do |tuple|
+      if re_lst.include?( tuple['course_name_ch'] )
+        #printf "%s not in tuple", tuple['course_name_ch']
+        return_lst << tuple
+      end
+    end
+
+    return return_lst
+    #return courses
+
+  end
+
   def search
 
-    query_dict = {
+    query_params = {
       'course_id'       => '1351',
       'semester'        => '1062',
       'subject_id'      => '258733002',
       'num_semesters'   => '2',
-      'course_name_ch'  => '賽局理論入門',
+      'course_name_ch'  => '賽局3',
       'course_name_en'  => 'An Introduction to Game Theory',
       'teacher'         => '何靜嫺',
       'credits'         => '3',
@@ -57,88 +130,26 @@ class CourseController < ApplicationController
       'updated_at'      => '0000-00-00 00:00:00'
     }
 
-    puts query_dict['department']
 
     name_lst = []
     # params[:query]
 
-    @like_result = Course.find_by_sql ["select course_name_ch from courses where course_name_ch LIKE ?", "%#{params[:query]}%" ]
+    @like_result = Course.find_by_sql ["select * from courses where course_name_ch LIKE ?", "%#{query_params['course_name_ch']}%" ]
 
     re_lst = []
 
     if @like_result == []
-
-      @name_hash = Course.select("course_name_ch").distinct
-      @name_hash.each do |n|
-        name_lst.push(n.course_name_ch)
-      end
-
-      # Build unique character lst
-      character_lst = []
-      name_lst.each { |word| word.split("").each { |c| character_lst << c  } }
-      character_lst = character_lst.uniq
-      counter = 0
-
-      # Build index mapping for vsm
-      index_mapping = {}
-      character_lst.each do |c|
-      	index_mapping[c] = counter
-      	counter += 1
-      end
-
-      #query clean
-      q = params[:query]
-      printf "Original Query: %s\n", q
-      penal = corpus_penalty(q, character_lst)
-      tmp_q = ""
-      q.split("").each {|c| if character_lst.include? c; tmp_q << c; end}
-      q = tmp_q
-      if tmp_q == ""
-        render :json => []
-        return
-      end
-
-
-      v2 = vectorize_tf(q, character_lst, index_mapping)
-      dt_score = {}
-      name_lst.each do |word|
-      	dt_score[word] = sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2) * penal
-      	#printf "%s: %.6f\n", word, sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2)
-      end
-
-
-      printf "Cleaned Query: %s\npenalty: %.5f\n\n", q, penal
-      lst_name_score = []
-      dt_score.each do |k, val|
-        tmp = [k, val]
-        lst_name_score.push( tmp )
-      end
-      sorted_lst_name_score = lst_name_score.sort_by { |tuple| tuple[1] }.reverse
-
-      sorted_lst_name_score.each do |t|
-        if t[1] > 0.45
-          re_lst.push( t[0] )
-          printf "%s:   %.4f\n", t[0], t[1]
-        else
-          break
-        end
-      end
+      re_lst = course_name_fuzzysearch(query_params['course_name_ch'])
     else
       @like_result.each do |dic|
-        re_lst.push(dic['course_name_ch'])
+        re_lst.push( dic )
       end
-
     end
-
 
     render :json => re_lst
 
-
-
-    #render :json => @name_lst
-
   end
-#'762', '1062', '101008002', '2', '中國文學史', 'History of Chinese Literature', '曾守正', '3', '09:10:00', '12:00:00', 'THU.', '研究250202', '中文二甲', '必', 'False', '', 'False', '0', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00'
+
   def index
 
     hash = {}
--- app/controllers/feature_controller.rb
+++ app/controllers/feature_controller.rb
@@ -33,85 +33,22 @@ class FeatureController < ApplicationController
 
   def search
 
-
-    name_lst = []
-    # params[:query]
-
-    @like_result = Course.find_by_sql ["select course_name_ch from courses where course_name_ch LIKE ?", "%#{params[:query]}%" ]
-
-    re_lst = []
-
-    if @like_result == []
-
-      @name_hash = Course.select("course_name_ch").distinct
-      @name_hash.each do |n|
-        name_lst.push(n.course_name_ch)
-      end
-
-      # Build unique character lst
-      character_lst = []
-      name_lst.each { |word| word.split("").each { |c| character_lst << c  } }
-      character_lst = character_lst.uniq
-      counter = 0
-
-      # Build index mapping for vsm
-      index_mapping = {}
-      character_lst.each do |c|
-      	index_mapping[c] = counter
-      	counter += 1
-      end
-
-      #query clean
-      q = params[:query]
-      printf "Original Query: %s\n", q
-      penal = corpus_penalty(q, character_lst)
-      tmp_q = ""
-      q.split("").each {|c| if character_lst.include? c; tmp_q << c; end}
-      q = tmp_q
-      if tmp_q == ""
-        render :json => []
-        return
-      end
-
-
-      v2 = vectorize_tf(q, character_lst, index_mapping)
-      dt_score = {}
-      name_lst.each do |word|
-      	dt_score[word] = sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2) * penal
-      	#printf "%s: %.6f\n", word, sim_cosine(vectorize_tf(word, character_lst, index_mapping), v2)
-      end
-
-
-      printf "Cleaned Query: %s\npenalty: %.5f\n\n", q, penal
-      lst_name_score = []
-      dt_score.each do |k, val|
-        tmp = [k, val]
-        lst_name_score.push( tmp )
-      end
-      sorted_lst_name_score = lst_name_score.sort_by { |tuple| tuple[1] }.reverse
-
-      sorted_lst_name_score.each do |t|
-        if t[1] > 0.45
-          re_lst.push( t[0] )
-          printf "%s:   %.4f\n", t[0], t[1]
-        else
-          break
-        end
-      end
-    else
-      @like_result.each do |dic|
-        re_lst.push(dic['course_name_ch'])
-      end
-
+    db_courses = Course.all
+    puts "----------------"
+    puts( db_courses.class )
+    puts "----------------"
+    courses = []
+    db_courses.each do |tuple|
+      courses << tuple
     end
+    puts "----------------"
+    puts( courses.class )
+    puts "----------------"
 
 
-    render :json => re_lst
-
-
-
-    #render :json => @name_lst
+    render :json => courses
 
   end
+
 
 end
