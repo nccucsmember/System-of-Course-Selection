35c8ce498072ce966550a698ff05d32ec0d07fe4
biceneutron <biceneutron@gmail.com>
2018-06-03 16:43:58 +0800

    Modify choose_controller.

--- app/controllers/choose_controller.rb
+++ app/controllers/choose_controller.rb
@@ -8,7 +8,7 @@ class ChooseController < ApplicationController
 
 		if @user
 			@user_id = @user.schoolid
-			@chosen_courses = Course.find_by_sql(['SELECT courses.* FROM courses, chooses WHERE is_chosen = true and student_id = ? and chooses.course_id = subject_id', @user_id])
+			@chosen_courses = Course.find_by_sql(['SELECT courses.* FROM courses, chooses WHERE is_chosen = 1 and student_id = ? and chooses.course_id = subject_id', @user_id])
 			render :json => {:chosen_courses_list => @chosen_courses, :message => 'OK'}
 		else
 			render :json => {:message => 'Invalid user'}
@@ -16,7 +16,7 @@ class ChooseController < ApplicationController
 	end
 
 	def update
-		### Set the isChosen field to opposite value
+		### Set the is_chosen field to opposite value
 
 		myhash = {:id => params['id'], :auth_token => request.headers['HTTP_AUTHORIZATION']}
 		@user = User.find_by_authentication_token(myhash[:auth_token])
@@ -27,7 +27,12 @@ class ChooseController < ApplicationController
 
 			choose = Choose.find_by(cs_id: @course_id + @user_id)
 			if choose != nil
-				choose.is_chosen = !choose.is_chosen
+				if chooses.is_chosen == 1
+					chooses.is_chosen = 0
+				else
+					chooses.is_chosen = 1
+				end
+
 				choose.save
 
 				@chosen_courses = Course.find_by_sql(['SELECT courses.* FROM courses, chooses WHERE is_chosen = true and student_id = ? and chooses.course_id = subject_id', @user_id])
