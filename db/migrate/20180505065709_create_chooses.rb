1f5dc52a5cc903fce1ca0a1b3a202f29dab25bf7
linzichao <joy10393@gmail.com>
2018-06-04 15:49:03 +0800

    debug

--- db/migrate/20180505065709_create_chooses.rb
+++ db/migrate/20180505065709_create_chooses.rb
@@ -1,9 +1,10 @@
 class CreateChooses < ActiveRecord::Migration[5.1]
   def change
     create_table :chooses, :id => false do |t|
-		  t.string :course_id, :null => false, primary_key: true
-		  t.string :student_id, :null => false, primary_key: true
+		  t.string :course_id, :null => false
+		  t.string :student_id, :null => false
       t.timestamps
     end
+    execute "ALTER TABLE chooses ADD PRIMARY KEY (course_id, student_id);"
   end
 end
