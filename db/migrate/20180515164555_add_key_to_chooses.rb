657db6f1e4278b883b14fcfd5f32c1636574940a
linzichao <joy10393@gmail.com>
2018-06-04 16:01:19 +0800

    debug

--- db/migrate/20180515164555_add_key_to_chooses.rb
+++ db/migrate/20180515164555_add_key_to_chooses.rb
@@ -1,5 +1,6 @@
 class AddKeyToChooses < ActiveRecord::Migration[5.1]
   def change
+    execute "ALTER TABLE `chooses` DROP PRIMARY KEY"
 	  change_column :chooses, :cs_id, :string, :primary_key => true
   end
 end
