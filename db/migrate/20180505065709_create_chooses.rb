540d86696f8decf035773fc463bca559aecd5aaa
linzichao <joy10393@gmail.com>
2018-06-04 15:43:01 +0800

    deploy travis

--- db/migrate/20180505065709_create_chooses.rb
+++ db/migrate/20180505065709_create_chooses.rb
@@ -1,9 +1,8 @@
 class CreateChooses < ActiveRecord::Migration[5.1]
   def change
-    create_table :chooses do |t|
-		t.string :course_id, :null => false, primary_key: true
-		t.string :student_id, :null => false, primary_key: true
-
+    create_table :chooses, :id => false do |t|
+		  t.string :course_id, :null => false, primary_key: true
+		  t.string :student_id, :null => false, primary_key: true
       t.timestamps
     end
   end
