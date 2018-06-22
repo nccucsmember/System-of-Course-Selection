d5a7cc5c6f7b1c80a2cdd2421db9b55d813744ae
biceneutron <biceneutron@gmail.com>
2018-06-22 23:15:47 +0800

    Fix "duplicate entry" and "email has been taken" situation.

--- spec/controllers/managecourse_spec.rb
+++ spec/controllers/managecourse_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ManagecourseController, type: :controller do
 
   describe "all feature of tracking system" do
 
-    let(:user) { User.create!(:email => 'joy1111@gmail.com', :password => 'aaaaaaa', :schoolid => "12121212") }
+    let(:user) { User.create!(:email => 'joy2222@gmail.com', :password => 'aaaaaaa', :schoolid => "34343434") }
     let(:course) { Course.create!(:course_id => "testcourseid", :semester => '1062', :subject_id => 'testid', :TA_id => '111') }
 
 
@@ -15,7 +15,7 @@ RSpec.describe ManagecourseController, type: :controller do
         request.headers["Authorization"] = "#{user.authentication_token}"
 
         course
-        track = Choose.create!( :cs_id => 'testid12121212', :course_id => 'testid', :student_id => '12121212', :is_chosen => '0' )
+        track = Choose.create!( :cs_id => 'testid34343434', :course_id => 'testid', :student_id => '34343434', :is_chosen => '0' )
 
         get :index
 
@@ -49,7 +49,7 @@ RSpec.describe ManagecourseController, type: :controller do
 
         request.headers["Authorization"] = "#{user.authentication_token}"
 
-        track = Choose.create!( :cs_id => 'testid12121212', :course_id => 'testid', :student_id => '12121212', :is_chosen => '0' )
+        track = Choose.create!( :cs_id => 'testid34343434', :course_id => 'testid', :student_id => '34343434', :is_chosen => '0' )
 
         delete :destroy, params: { id: course.subject_id }
 
