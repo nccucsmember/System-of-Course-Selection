48644ada0126dc9a81f74149d542e0d92bc53e9d
biceneutron <biceneutron@gmail.com>
2018-06-23 16:39:25 +0800

    Fix test ta model bug.

--- spec/models/ta_spec.rb
+++ spec/models/ta_spec.rb
@@ -3,31 +3,40 @@ require 'rails_helper'
 RSpec.describe Ta do
 
 	context 'when there is a ta record in database' do
+=begin
 		before(:all) do
 			@ta = Ta.new(:TA_id => '789',
 						 :name => 'test_name',
 						 :email => 'test@test.com')
-			@course = Course.new(:course_id => 'testid',
+			@course = Course.create(:course_id => 'testid',
 									:semester => '1062',
 								    :subject_id => 'testid',
 								    :TA_id => @ta.TA_id)
 		end
-
+=end
+		
+		let(:ta) { Ta.create!(:TA_id => '789',
+					 		  :name => 'test_name',
+					 		  :email => 'test@test.com')}
+		let(:course) { Course.create!(:course_id => 'testid',
+							 	      :semester => '1062',
+							    	  :subject_id => 'testid',
+							    	  :TA_id => ta.TA_id)}
 		it 'should have a ta id' do
-			expect(@ta.TA_id).not_to be_empty
+			expect(ta.TA_id).not_to be_empty
 		end
 
 		it 'should have name' do
-			expect(@ta.name).not_to be_empty
+			expect(ta.name).not_to be_empty
 		end
 
 		it 'should have email' do
-			expect(@ta.email).not_to be_empty
+			expect(ta.email).not_to be_empty
 		end
 
 		it "has_many courses" do
 			#post = Post.create(:title => "title", :content => "content")
-			expect(@ta.courses).to include(@course)
+			expect(ta.courses).to include(course)
 		end
 	end
 end
