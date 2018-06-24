e9643dd69e0e5e93e6fe9f5e48cd83f70045a258
dream85123 <104703051@nccu.edu.tw>
2018-06-24 14:39:46 +0800

    check_comment to all courses have same name and teacher

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -122,26 +122,31 @@ class CommentController < ApplicationController
 
   def check_thumbup
 
-    myhash = {:auth_token => request.headers["HTTP_AUTHORIZATION"]}
-    @user = User.find_by_authentication_token(myhash[:auth_token])
-    
-    if @user
-      @user_id = @user.schoolid
-      @comment = Comment.where(:subject_id => params[:id])
-      comment = @comment.as_json
-      comment.each {
-        |c|
-        if Thumbup.find_by(auth_token:myhash[:auth_token], comment_id:c["id"]) == nil
-          c["have_thumbup"] = false
-        else
-          c["have_thumbup"] = true
-        end
-      }
-      render :json => comment
-    else
-      render :json => {:message =>"Invalid user!"}
-    end
+    if Course.find_by(:subject_id => params[:id])!=nil
+        course_name_ch_now = Course.find_by(:subject_id => params[:id]).course_name_ch
+        teacher_now = Course.find_by(:subject_id => params[:id]).teacher
 
+        myhash = {:auth_token => request.headers["HTTP_AUTHORIZATION"]}
+        @user = User.find_by_authentication_token(myhash[:auth_token])
+        #@user_id = @user.schoolid
+
+        if @user
+            @user_id = @user.schoolid
+            @comment = Comment.find_by_sql(["select comments.*,semester,num_semesters from comments,courses where comments.subject_id=courses.subject_id and course_name_ch=? and teacher=? order by comments.id desc",course_name_ch_now,teacher_now])
+            comment = @comment.as_json
+            comment.each {
+              |c|
+              if Thumbup.find_by(auth_token:myhash[:auth_token], comment_id:c["id"]) == nil
+                c["have_thumbup"] = false
+              else
+                c["have_thumbup"] = true
+              end
+            }
+            render :json => comment
+        else
+          render :json => {:message =>"Invalid user!"}
+        end
+    end
   end
 
   def click_thumbup
